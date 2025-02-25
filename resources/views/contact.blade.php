c94a157269b8dacd78c70c9fca64686de6060870
Gondain <Quueenntiin@gmail.com>
2025-02-25 09:34:36 +0100

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <x-slot:title>
+        Contact
+    </x-slot:title>
+    <x-slot:heading>
+        Contact page
+    </x-slot:heading>
+    <h1>Contact page</h1>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <x-slot:title>
+        Home
+    </x-slot:title>
+    <x-slot:heading>
+        Home page
+    </x-slot:heading>
+    <h1>Home page</h1>
+</x-layout>
